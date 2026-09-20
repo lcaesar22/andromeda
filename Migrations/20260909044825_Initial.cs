bacc02553e23f0ec3d765d259fbe312cdeb19255
lcaesar22 <lionelsamcaesartwumson@gmail.com>
2026-09-20 05:23:36 +0000

    Add files via upload

---
+++ Migrations/20260909044825_Initial.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Andromeda.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Satellites",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: false),
+                    NoradId = table.Column<int>(type: "integer", nullable: false),
+                    Type = table.Column<int>(type: "integer", nullable: false),
+                    LaunchDate = table.Column<DateOnly>(type: "date", nullable: false),
+                    Orbit = table.Column<int>(type: "integer", nullable: false),
+                    CountryOfOrigin = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    IsActive = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Satellites", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Satellites_NoradId",
+                table: "Satellites",
+                column: "NoradId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Satellites");
+        }
+    }
+}
